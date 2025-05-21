ec2aca971ff0a6edcc837456d30973ad6a03e77a
pilonull <pilonull@users.noreply.github.com>
2025-05-21 14:57:40 -0400

    Update adurosmart.ts
    Add support for 81812-HO AD-CTW123001

--- src/devices/adurosmart.ts
+++ src/devices/adurosmart.ts
@@ -117,6 +117,13 @@ export const definitions: DefinitionWithExtend[] = [
         extend: [m.light({colorTemp: {range: [153, 500]}, color: {modes: ["xy", "hs"]}})],
     },
     {
+        zigbeeModel: ['AD-CTW123001'],
+        model: 'AD-CTW123001',
+        vendor: 'AduroSmart',
+        description: 'Eria tunable white A19/BR30 smart bulb',
+        extend: [m.light({"colorTemp":{"range":[153,500]}})],
+    },
+    {
         zigbeeModel: ["ONOFFRELAY"],
         model: "81898",
         vendor: "AduroSmart",
